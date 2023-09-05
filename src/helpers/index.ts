65df2803c6a150406e9040a6872cf7f358a47126
greedythib <thibaud.bruyelle@gmail.com>
2023-09-05 16:56:37 +0200

    feat: camelot support

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,11 +1,12 @@
-import { AMMType, Erc20__factory, Multicall__factory, UniswapV3Pool__factory } from '@angleprotocol/sdk';
+import { AMMAlgorithmMapping, AMMAlgorithmType, AMMType, Erc20__factory, Multicall__factory } from '@angleprotocol/sdk';
 
 import { httpProvider } from '../providers';
+import { PoolInterface } from '../types';
 
 export const fetchPoolName = async (chainId: number, pool: string, amm: AMMType) => {
   const provider = httpProvider(chainId);
   const multicall = Multicall__factory.connect('0xcA11bde05977b3631167028862bE2a173976CA11', provider);
-  const poolInterface = UniswapV3Pool__factory.createInterface();
+  const poolInterface = PoolInterface(AMMAlgorithmMapping[amm]);
   const erc20Interface = Erc20__factory.createInterface();
 
   let calls = [
@@ -19,17 +20,24 @@ export const fetchPoolName = async (chainId: number, pool: string, amm: AMMType)
       target: pool,
       allowFailure: false,
     },
-    {
-      callData: poolInterface.encodeFunctionData('fee'),
-      target: pool,
-      allowFailure: false,
-    },
+    ...(AMMAlgorithmMapping[amm] === AMMAlgorithmType.UniswapV3
+      ? [
+          {
+            callData: poolInterface.encodeFunctionData('fee'),
+            target: pool,
+            allowFailure: false,
+          },
+        ]
+      : null),
   ];
   let res = await multicall.callStatic.aggregate3(calls);
-  const token0 = poolInterface.decodeFunctionResult('token0', res[0].returnData)[0];
-  const token1 = poolInterface.decodeFunctionResult('token1', res[1].returnData)[0];
-  const fee = poolInterface.decodeFunctionResult('fee', res[2].returnData)[0];
-
+  let i = 0;
+  const token0 = poolInterface.decodeFunctionResult('token0', res[i++].returnData)[0];
+  const token1 = poolInterface.decodeFunctionResult('token1', res[i++].returnData)[0];
+  let fee;
+  if (AMMAlgorithmMapping[amm] === AMMAlgorithmType.UniswapV3) {
+    fee = poolInterface.decodeFunctionResult('fee', res[i].returnData)[0];
+  }
   calls = [
     {
       callData: erc20Interface.encodeFunctionData('symbol'),
@@ -46,26 +54,7 @@ export const fetchPoolName = async (chainId: number, pool: string, amm: AMMType)
   const token0Symbol = erc20Interface.decodeFunctionResult('symbol', res[0].returnData)[0];
   const token1Symbol = erc20Interface.decodeFunctionResult('symbol', res[1].returnData)[0];
 
-  return `${AMMType[amm]} ${token0Symbol}-${token1Symbol}-${fee}`;
+  return `${AMMType[amm]} ${token0Symbol}-${token1Symbol}-${fee ?? ``}`;
 };
 
 export const round = (n: number, dec: number) => Math.round(n * 10 ** dec) / 10 ** dec;
-
-export declare type PositionType = {
-  endTimestamp: number;
-  owner: string;
-  startTimestamp: number;
-  tickLower: number;
-  tickUpper: number;
-  liquidity: string;
-};
-
-export type SwapType = {
-  amount0: string;
-  amount1: string;
-  amountUSD: string;
-  tick: string;
-  sqrtPriceX96: string;
-  timestamp: string;
-  transaction: { blockNumber: string };
-};
