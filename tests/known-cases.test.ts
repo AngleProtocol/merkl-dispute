a8f8a60008b1a299c5583f0ff49bf6c245076d03
clement4saunier <clement4saunier@gmail.com>
2023-09-25 16:12:31 +0200

    add: know error cases in test

--- tests/known-cases.test.ts
+++ tests/known-cases.test.ts
@@ -12,13 +12,30 @@ type ProblematicBlock = {
 
 describe('Known cases of past disputes', async function () {
   it('Should output same errors cases from Polygon history', async function () {
-    const polygonProblematicBlocks: ProblematicBlock[] = [{ blockNumber: 47471300, errorCode: ERROR_TREE_NEGATIVE_DIFF }];
-
-    const testContext: DisputeContext = defaultContext(137, 47471300);
-    testContext.logger = undefined;
+    const polygonProblematicBlocks: ProblematicBlock[] = [
+      { blockNumber: 47471300, errorCode: ERROR_TREE_NEGATIVE_DIFF },
+      { blockNumber: 47456455, errorCode: ERROR_TREE_NEGATIVE_DIFF },
+    ];
 
     await Promise.all(
       polygonProblematicBlocks.map(async (block) => {
+        const testContext: DisputeContext = defaultContext(137, block.blockNumber);
+        testContext.logger = undefined;
+        const { error, code } = await checkDisputeOpportunity(testContext);
+
+        expect(error).to.equal(true);
+        expect(code).to.equal(block.errorCode);
+      })
+    );
+  });
+
+  it('Should output same errors cases from Ethereum history', async function () {
+    const ethereumProblematicBlocks: ProblematicBlock[] = [{ blockNumber: 17812800, errorCode: ERROR_TREE_NEGATIVE_DIFF }];
+
+    await Promise.all(
+      ethereumProblematicBlocks.map(async (block) => {
+        const testContext: DisputeContext = defaultContext(1, block.blockNumber);
+        testContext.logger = undefined;
         const { error, code } = await checkDisputeOpportunity(testContext);
 
         expect(error).to.equal(true);
