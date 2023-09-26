e02395a925c97214264f9a6cb16b0076b5010053
clement4saunier <clement4saunier@gmail.com>
2023-09-26 10:49:57 +0200

    add: already claimed tests for end of august period

--- tests/known-cases.test.ts
+++ tests/known-cases.test.ts
@@ -8,24 +8,28 @@ import { checkDisputeOpportunity } from '../src/bot/run';
 type ProblematicBlock = {
   chainId: number;
   blockNumber: number;
-  errorCode: number;
+  errorCode?: number;
 };
 
-const tryAtBlock = async (block: ProblematicBlock) => {
-  const testContext: DisputeContext = defaultContext(block.chainId, block.blockNumber);
+const tryAtBlock = async ({chainId, blockNumber, errorCode}: ProblematicBlock) => {
+  const testContext: DisputeContext = defaultContext(chainId, blockNumber);
   testContext.logger = undefined;
   const { error, code, reason } = await checkDisputeOpportunity(testContext);
+  const testForError = !!errorCode;
 
-  console.log(`reason on ${block.chainId} at ${block.blockNumber}`, reason);
-  expect(error).to.equal(true);
-  expect(code).to.equal(block.errorCode);
+  console.log(`reason on ${chainId} at ${blockNumber}`, reason);
+
+  expect(error).to.equal(testForError);
+  testForError && expect(code).to.equal(errorCode);
 };
 
 describe('Known cases of past disputes', async function () {
   it('Should output same errors cases from Merkl history', async function () {
     const problematicBlocks: ProblematicBlock[] = [
-      { chainId: 137, blockNumber: 47471300, errorCode: DisputeError.NegativeDiff },
       { chainId: 1, blockNumber: 17812800, errorCode: DisputeError.NegativeDiff },
+      { chainId: 1, blockNumber: 18013500, errorCode: DisputeError.AlreadyClaimed },
+      { chainId: 1, blockNumber: 18052100, errorCode: DisputeError.AlreadyClaimed },
+      { chainId: 1, blockNumber: 18059300, errorCode: undefined },
     ];
 
     await Promise.all(problematicBlocks.map(tryAtBlock));
