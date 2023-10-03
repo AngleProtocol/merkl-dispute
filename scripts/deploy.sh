3163a6aff40b38c488ca1f11bbfb8d3178d6b061
Baptiste Guerin <baptist.guerin@gmail.com>
2023-10-03 10:22:57 +0200

    feat: make deployments faster

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,41 +1,31 @@
 #!/bin/bash
 
-
-
-echo "Please enter the version to deploy in the following format: vX.X.X "
-read version
-
-accounts=("merkl-dispute-1") #"merkl-dispute-2")
+account=$1
+version=$2
 
 chainKeys=("polygon" "ethereum" "optimism" "arbitrum" "zkevm" "base")
 chainValues=(137 1 10 42161 1101 8453)
 
-for account in "${accounts[@]}"
+for ((i=0; i<${#chainKeys[@]}; i++))
 do
-    gcloud auth login
-    gcloud config set project $account
+    chain=${chainKeys[$i]}
 
-    for ((i=0; i<${#chainKeys[@]}; i++))
-    do
-        chain=${chainKeys[$i]}
+    echo "Updating merkl-dispute-${chain} to $account with version $version"
 
-        echo "Updating merkl-dispute-${chain} to $account with version $version"
+    cp scripts/templates/cloudrun.yaml ./cloudrun.yaml
 
-        cp scripts/templates/cloudrun.yaml ./cloudrun.yaml
+    export APP_NAME=merkl-dispute-$chain
+    yq -i '.metadata.name=strenv(APP_NAME)' ./cloudrun.yaml
 
-        export APP_NAME=merkl-dispute-$chain
-        yq -i '.metadata.name=strenv(APP_NAME)' ./cloudrun.yaml
+    export IMAGE=europe-west1-docker.pkg.dev/$account/registry/merkl-dispute:$version
+    yq -i '.spec.template.spec.containers[0].image= strenv(IMAGE)' ./cloudrun.yaml
 
-        export IMAGE=europe-west1-docker.pkg.dev/$account/registry/merkl-dispute:$version
-        yq -i '.spec.template.spec.containers[0].image= strenv(IMAGE)' ./cloudrun.yaml
+    export CHAIN_ID=${chainValues[$i]}
+    yq -i '.spec.template.spec.containers[0].env[0].value= strenv(CHAIN_ID)' ./cloudrun.yaml
 
-        export CHAIN_ID=${chainValues[$i]}
-        yq -i '.spec.template.spec.containers[0].env[0].value= strenv(CHAIN_ID)' ./cloudrun.yaml
+    export SERVICE_ACCOUNT=merkl-dispute-sa@$account.iam.gserviceaccount.com
+    yq -i '.spec.template.spec.serviceAccountName= strenv(SERVICE_ACCOUNT)' ./cloudrun.yaml
 
-        export SERVICE_ACCOUNT=merkl-dispute-sa@$account.iam.gserviceaccount.com
-        yq -i '.spec.template.spec.serviceAccountName= strenv(SERVICE_ACCOUNT)' ./cloudrun.yaml
-
-        gcloud run services replace ./cloudrun.yaml --platform managed --region europe-west1
-        rm ./cloudrun.yaml
-    done
+    gcloud run services replace ./cloudrun.yaml --platform managed --region europe-west1
+    rm ./cloudrun.yaml
 done
